a6355f63926c1c33cc880bf8e5a0ceceda45e18a
Alexa Pham <alexapham01@gmail.com>
2022-11-08 22:11:44 -0800

    Display calendar grid
    Imported 'react-big-calendar' package to have a calendar component.
    Put the appropriate code under the <div> tag to display on the front end.
    Added styling elements to App.css to make sure the calendar component
    is presented as a grid calendar.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import "./App.css";
 import React, { Component } from "react";
+import "./App.css";
 import {logInWithGoogle} from "./Firebase";
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
+const localizer = momentLocalizer(moment)
 
-class App extends Component {
-  render() {
-      return (
-        <div>
-          <h1>Stalk;</h1>
-          <ul className="header">
-            <button onClick={logInWithGoogle}>Login with Google</button>
-          </ul>
-          <div className="content">
-          </div>
+function App() {
+    return (
+      <div>
+        <h1>Stalk;</h1>
+        <ul className="header">
+          <button onClick={logInWithGoogle}>Login with Google</button>
+        </ul>
+        <div className="content">
+            <Calendar
+              localizer={localizer}
+              events={[]}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 500 }}
+            />
         </div>
-    );
-  }
-
-}
-
+      </div>
+  );
+};
+ 
 export default App;
