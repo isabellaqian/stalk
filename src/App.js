82223ece98524ed1acd63b012255ba4d1d4f9487
Swetha Palakur <spalakur@g.ucla.edu>
2022-11-10 03:51:33 -0800

    Created manual entry to add calendar events from the web app for early Firebase Realtime Database integration. Linked Realtime database to app and attempted sending title and dates of event to RDB.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
-import {logInWithGoogle} from "./Firebase";
+import {logInWithGoogle, handleClick} from "./Firebase";
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+//trying out a date-picker (@s-palakur)
+import DatePicker from 'react-date-picker';
 
 const localizer = momentLocalizer(moment)
 
 function App() {
+  //creating objects with state to represent event info
+  const [eventTitle, setTitle] = useState('');
+  const [date, onChange] = useState(new Date());
+
     return (
       <div>
         <h1>Stalk;</h1>
         <ul className="header">
           <button onClick={logInWithGoogle}>Login with Google</button>
+          <h2>Enter your calendar event below!</h2>
+          <form>
+            <label>Event name:</label>
+            <input
+              type="text"
+              required
+              value={eventTitle}
+              onChange={(a) => setTitle(a.target.value)} //constantly updates the state
+              />
+              <label>Event date:</label>
+              <DatePicker 
+              onChange={onChange} 
+              value={date} />
+              <button onClick={handleClick(eventTitle, date)}>Create event!</button> 
+          </form>
         </ul>
         <div className="content">
             <Calendar
--- src/Firebase.js
+++ src/Firebase.js
@@ -5,6 +5,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+//Configuring Realtime Firebase database(@s-palakur)
+import { getDatabase , ref, set } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +20,9 @@ const firebaseConfig = {
   storageBucket: "stalk-6c0fe.appspot.com",
   messagingSenderId: "838823308350",
   appId: "1:838823308350:web:240854399139887031b56f",
-  measurementId: "G-DTE6CWB5N2"
+  measurementId: "G-DTE6CWB5N2",
+  //added database URL - set rules to true by default
+  databaseURL: "https://stalk-6c0fe-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
@@ -25,6 +30,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 
 const provider = new GoogleAuthProvider()
+const database = getDatabase();
 
 // add scopes for Calendar API (what we want to access from user's Google Account)
 provider.addScope('https://www.googleapis.com/auth/calendar');
@@ -41,4 +47,31 @@ export const logInWithGoogle = () => {
     .catch((error) => {
         console.log(error);
     });
+    const user = auth.currentUser;
+
+// Added this to get user info (@s-palakur) - remove necessary info later
+//This is within this function to get auth after user logs in
+if (user !== null) {
+  // The user object has basic properties such as display name, email, etc.
+  const displayName = user.displayName;
+  const email = user.email;
+  const photoURL = user.photoURL;
+  const emailVerified = user.emailVerified;
+
+  // The user's ID, unique to the Firebase project. Do NOT use
+  // this value to authenticate with your backend server, if
+  // you have one. Use User.getToken() instead.
+  const uid = user.uid;
+}
 };
+
+export function handleClick (t, d) {
+    const db = getDatabase(); //still have to figure out how to pass in the email
+    //make it mandatory to sign in
+    set(ref(db), {
+    Title: t,
+    Date: d
+  });
+}
+
+
