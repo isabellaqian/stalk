61fad55b532128abdf1cb15b6af0272be52b93bd
isabellaqian <isabellaqian@hotmail.com>
2022-11-10 23:44:56 -0800

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Landing from "./landing.js";
 
 function App() {
   return (
-    <div className="App">
-      <h>hi there</h>
-    </div>
+    // <div className="App">
+    //   <h>hi there</h>
+    // </div>
+    <Landing />
   );
 }
-
 export default App;
---
+++ src/landing.js
@@ -0,0 +1,37 @@
+import React from "react";
+import calendar_screenshot from "./images/calendar_screenshot.png";
+import logo from "./images/logo.png";
+export default function landing() {
+  return (
+    <div>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Oxygen+Mono&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Oxygen&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <div class="nav_bar">
+        <img src={logo} class="logo" />
+        <button
+          class="button_accent"
+          onClick={console.log("clicked on sign up")}
+        >
+          Sign up now
+        </button>
+        <button class="button_white" onClick={console.log("clicked on login")}>
+          Log in
+        </button>
+      </div>
+      <div class="container">
+        <body class="landing_text">
+          Schedule meetings with ease. Schedules at a glance.
+        </body>
+        <img src={calendar_screenshot} alt="calendar view" class="hero" />
+      </div>
+    </div>
+  );
+}
