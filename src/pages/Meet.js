71a238325e4cc80d5a1b8ad95acc50256b02757a
isabellaqian <isabellaqian@hotmail.com>
2022-11-26 23:18:50 -0800

    minor div adjustments in Meet.js

--- src/pages/Meet.js
+++ src/pages/Meet.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import arrow from "../images/left_arrow.png";
-import Multiselect from 'multiselect-react-dropdown';
+import Multiselect from "multiselect-react-dropdown";
 
 const Meet = () => {
-
   //realisitcally, we need to get the user's friend list from the database (@alexavanh)
   const [selectedFriends, setSelectedFriends] = useState([]);
   const friendsList = ["Alexa", "Izzy", "Swetha", "Amy", "Emily"];
@@ -21,22 +20,22 @@ const Meet = () => {
           </Link>
           <div className="h3">Meet with your friends!</div>
         </div>
+        <div className="smallerh3 padding">
+          <h1>Choose friends to meet up with!</h1>
+        </div>
+        <div className="multiselect">
+          <Multiselect
+            isObject={false}
+            onRemove={(event) => {
+              console.log(event);
+            }}
+            onSelect={handleSelect}
+            options={friendsList}
+            selectedValues={selectedFriends}
+            placeholder="Select friends!"
+          />
+        </div>
       </div>
-      <div className="smallerh3 padding">
-        <h1>Choose friends to meet up with!</h1>
-      </div>
-      <div className="multiselect">
-        <Multiselect
-          isObject={false}
-          onRemove={(event) => {
-            console.log(event);
-          }}
-          onSelect={handleSelect}
-          options={friendsList}
-          selectedValues={selectedFriends}
-          placeholder="Select friends!"
-        />
-      </div>    
     </div>
   );
 };
