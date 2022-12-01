99d776100f89515c212256304c15f4922ecc343a
Alexa Pham <alexapham01@gmail.com>
2022-11-30 19:28:54 -0800

    Forgot to add FriendList component to previous merge commit

---
+++ src/components/FriendList.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { userCollection } from "../Firebase";
+import { onSnapshot } from 'firebase/firestore'
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import FolderIcon from '@mui/icons-material/Folder';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { collection } from "firebase/firestore";
+import { getID, firestore } from "../Firebase";
+
+
+
+const FriendList = () => {
+    const [friendArr, setFriendArr] = useState([]);
+    const [dense, setDense] = React.useState(false);
+    const [secondary, setSecondary] = React.useState(false);
+
+    useEffect(() => {
+        //Get all users here (@emily)
+        const friendCollection = collection(
+          firestore,
+          "userCollection/" + getID() + "/friends"
+        );
+        const friendsArrFirestore = [];
+        const unsubscribe = onSnapshot(friendCollection, (snapshot) => {
+          snapshot.forEach((doc) => {
+            friendsArrFirestore.push(doc.data().emailID);
+          });
+          setFriendArr(friendsArrFirestore);
+        });
+        return () => unsubscribe();
+      }, []);
+
+    //generates the number of friends to list out
+    function generate(element) {
+        return friendArr.map((friend) =>
+          React.cloneElement( 
+            <ListItemText primary={friend}></ListItemText>
+            ),
+        );
+      }
+
+    return (
+        <div className="friend-list">
+            <Typography variant="h4">
+                Friend List
+            </Typography>
+            <List dense={dense}>
+              {generate(
+                <ListItem>
+                  <ListItemText/>
+                </ListItem>
+              )}
+            </List>
+        </div>
+    );
+}
+
+export default FriendList;
