6bad69143365aa71a36e1745850fb6d7ab6fc4f3
isabellaqian <isabellaqian@hotmail.com>
2022-12-02 03:00:19 -0800

    chip color

--- src/pages/Meet.js
+++ src/pages/Meet.js
@@ -183,6 +183,10 @@ const Meet = () => {
             chips: {
               background: "#e05927",
             },
+            option: {
+              // To change css for dropdown options
+              background: "#ECAD96",
+            },
           }}
         />
         <TextField
